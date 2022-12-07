9262a91478f719cb2820e248dfb968e76a773376
naocod <naocod@github.com>
2022-12-07 18:13:26 +0900

    no message

--- src/main/resources/static/js/support_pay/support_step1.js
+++ src/main/resources/static/js/support_pay/support_step1.js
@@ -1,60 +1,46 @@
 const selected = document.querySelectorAll(".support-selects");
-const layer = document.querySelectorAll(".row2");
+    const layer = document.querySelectorAll(".row2");
 
-selected.forEach((select,index) => {
-  select.onclick = () => {
-    layer[index].classList.toggle("invisible-area");
+    selected.forEach((select,index) => {
+      select.onclick = () => {
+        layer[index].classList.toggle("invisible-area");
+      }
+    });
+
+function printValue() {
+  const inputValue = document.getElementById('regular-input');
+  document.getElementById("donationPay").innerHTML = inputValue.value * 30000;
+
+}
+
+class SelectValue {
+
+  constructor() {
+    this.getCheckboxValue();
+    this.getInputValue();
   }
-});
-
-// function getCheckboxValue()  {
-//   // // 선택된 목록 가져오기
-//   // const query = 'input[name="regular-1"]:checked';
-//   // const selectedEls = 
-//   //     document.querySelectorAll(query);
-  
-//   // // 선택된 목록에서 value 찾기
-//   let result = '';
-//   // selectedEls.forEach((el) => {
-//   //   result = el.value + ' ';
-//   // });
-  
-//   // // 출력
-
-// }
 
 
-  class SelectValue {
-
-    constructor() {
-      // this.getdonationvaule();
-      this.getCheckboxValue();
-    }
-
-    getdonationvaule() {
-      const regular1 = document.getElementById("regular-1-1").value;
-      const donatepay = document.querySelector(".donationPay");
-
-      document.getElementById('donationPay').innerText= Number(regular1) * Number(30000);
-      
-    }
-
-    getCheckboxValue() {
-      let regular1 = document.getElementsByName("regular-1");
-      console.log(regular1[1].value);
-      const donatepay = document.querySelector(".donationPay");
-      for (let i = 0; i < regular1.length-1; i++) {
-        regular1[i].onclick = () => {
-          if(regular1[i].checked) {
-            document.getElementById('donationPay').innerText = Number(regular1[i].value * 30000);
-          }
+  getCheckboxValue() {
+    let regular1 = document.getElementsByName("regular-1");
+    let donatePay = document.getElementsByName("donationPay");
+      for(let j = 0; j < regular1.length; j++) {
+        regular1[j].onclick = () => {
+          console.log(Number(regular1[j].value))
+          if(regular1[j].checked) {
+            donatePay[parseInt(j/3)].innerHTML = Number(regular1[j].value) * Number(30000);
         }
       }
-      
     }
-
   }
 
+  getInputValue() {
+    let donatePay = document.getElementsByName("donationPay");
+    let regularInput = document.getElementById("regular-input").value;
+
+
+  }
+}
   window.onload = () => {
     new SelectValue();
   }
